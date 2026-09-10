7bdfb653fbe1fde089795d54b404f4cf1290ac29
danielbeuting <daniel@beut.nl>
2026-09-10 13:21:58 +0200

    Fix beheer route to open new settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, useLocation } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import ManageV2 from './ManageV2.jsx'
 import '../style.css'
 import './v2-bridge.css'
 import './feedback-preview.css'
+import './feedback-preview.js'
 import './manage-v2.css'
-
-function Entry(){
-  const location=useLocation()
-  if(/^\/app\/[^/]+\/?$/.test(location.pathname)) return <ManageV2 />
-  return <App />
-}
 
 if ('serviceWorker' in navigator && import.meta.env.PROD) {
   window.addEventListener('load', () => navigator.serviceWorker.register('/service-worker.js').catch(() => {}))
 }
 
+const isManageRoute = /^\/app\/[^/]+\/?$/.test(window.location.pathname)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Entry />
+      {isManageRoute ? <ManageV2 /> : <App />}
     </BrowserRouter>
   </React.StrictMode>,
 )
