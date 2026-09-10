2d3740d62a8c106c612157a0fe65cbef41c144f0
danielbeuting <daniel@beut.nl>
2026-09-10 10:59:17 +0200

    Load mobile-first V2 layout overrides

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles.css'
+import './mobile.css'
 
 if ('serviceWorker' in navigator && import.meta.env.PROD) {
   window.addEventListener('load', () => navigator.serviceWorker.register('/service-worker.js').catch(() => {}))
