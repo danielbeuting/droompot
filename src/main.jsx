9235e98f6d62cdea78d715ef4fa2ba4bb2a8b9ee
danielbeuting <daniel@beut.nl>
2026-09-10 13:05:31 +0200

    Route settings to rebuilt feedback implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import App from './App.jsx'
+import ManageV2 from './ManageV2.jsx'
 import '../style.css'
 import './v2-bridge.css'
 import './feedback-preview.css'
-import './feedback-preview.js'
+import './manage-v2.css'
+
+function Entry(){
+  const location=useLocation()
+  if(/^\/app\/[^/]+\/?$/.test(location.pathname)) return <ManageV2 />
+  return <App />
+}
 
 if ('serviceWorker' in navigator && import.meta.env.PROD) {
   window.addEventListener('load', () => navigator.serviceWorker.register('/service-worker.js').catch(() => {}))
@@ -14,7 +21,7 @@ if ('serviceWorker' in navigator && import.meta.env.PROD) {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Entry />
     </BrowserRouter>
   </React.StrictMode>,
 )
